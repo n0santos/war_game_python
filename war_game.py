743277af2dbf174e17a9a6d7ef6c1d69f400052e
n0santos <nicoyhenard@gmail.com>
2022-05-25 14:00:25 -0300

    Created python file and card_values dict.

---
+++ war_game.py
@@ -0,0 +1,17 @@
+'''
+Second milestone project of Complete Python Bootcamp Udemy course.
+Author: Nicolau Santos
+'''
+
+# Import libraries
+import random 
+
+# Create card_values dictionary
+
+card_values = {"ace":14, "king": 13, "queen":12, "jack":11, "ten":10, "nine":9, "eight":8, "seven":7, "six":6, "five":5, "four":4, "three":3, "two":2}
+
+# Create Card Class
+
+# Create Deck Class
+
+# Create Player Class
