8f64176becf5e2ba0e221fec2fb0a7748d1c3c9d
n0santos <nicoyhenard@gmail.com>
2022-05-26 18:20:04 -0300

    Added ranks and suits variables

--- war_game.py
+++ war_game.py
@@ -7,7 +7,8 @@ Author: Nicolau Santos
 import random 
 
 # Create card_values dictionary
-
+ranks = ["ace", "king", "queen", "jack", "ten", "nine", "eight", "seven", "six", "five", "four", "three", "two"]
+suits = ["Hearts","Spades","Diamonds","Clubs"]
 card_values = {"ace":14, "king": 13, "queen":12, "jack":11, "ten":10, "nine":9, "eight":8, "seven":7, "six":6, "five":5, "four":4, "three":3, "two":2}
 
 # Create Card Class
@@ -30,5 +31,18 @@ class Card:
         return f"{self.rank} of {self.suit}"
 
 # Create Deck Class
+
+class Deck:
+    '''
+    Class for deck objects.
+
+    Attributes: number(?)
+    Methods: create, shuffle, deal
+    '''
+
+    def __init__(self, number):
+        self.number = number
+    
+    def create(self)
 
 # Create Player Class
